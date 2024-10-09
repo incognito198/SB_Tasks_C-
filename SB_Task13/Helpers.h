74b50e933ed5547b88fd942eb23afacd174dbf47
incognito198 <incognito119988@gmail.com>
2024-10-09 13:25:05 +0300

    Добавить HEAD. Загрузить задание модуля 13

---
+++ SB_Task13/Helpers.h
@@ -0,0 +1,5 @@
+
+int FuncSrt(int a, int b)
+{
+	return (a + b) * (a + b);
+}
---
+++ SB_Task13/SB_Task13.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int result = FuncSrt(9, 11);
+    std::cout << "Result = " << result;
+}
