831d14523656c0aea6de7c3a5710f46ed1975d6c
incognito198 <incognito119988@gmail.com>
2024-10-09 14:21:51 +0300

    Поменять название

---
+++ SB_Task14/ConsoleApplication1/Task_14.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    std::string strVar = "Task 14 test string";
+    SetConsoleOutputCP(1251);
+    std::cout << "Строка: " << strVar << "\n" << "Длина строки: " << strVar.length() << "\n" << "Первый символ строки : " << strVar.front() << "\n" << "Последний символ строки : " << strVar.back();
+}
+
